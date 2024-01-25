d5c37da3eb46a11182dedee54616e87c609ff540
angelabernabeo <angela.bernabeo@baruchmail.cuny.edu>
2024-01-25 17:51:00 -0500

    pause button first draft

--- Changing_Perspectives/function.js
+++ Changing_Perspectives/function.js
@@ -3,3 +3,10 @@ $(document).ready(function(){
         interval: 10000
     });
 });
+
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce))");
+const details = document.querySelector(".object-and-details > details");
+
+if (mediaQuery.matches) {
+  details.removeAttribute("open");
+}
